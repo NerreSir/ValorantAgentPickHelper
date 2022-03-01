611eb273f0343ab9ee2d39cf47a9efb78b82cbaa
nerresir <bugraarslan96@gmail.com>
2022-03-01 17:18:23 +0300

    ValorantAgentPickHelper V0.1.1

--- app/src/main/java/com/liontertainment/agentpicker/MainActivity.java
+++ app/src/main/java/com/liontertainment/agentpicker/MainActivity.java
@@ -4,31 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button Buttoncontroller;
+    Button Buttoninitiator;
+    Button Buttonduelist;
+    Button Buttonsentinel;
+    Random r;
+    Integer[] imagescontroller = {
+            R.drawable.astrav3,
+            R.drawable.bbrimstone,
+            R.drawable.viper,
+            R.drawable.omen,
+    };
+
+    Integer[] imagessentinel = {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        imageView = (ImageView) findViewById(R.id.valogoimageView);
+        Buttoncontroller = (Button) findViewById(R.id.controllerbutton);
+        r = new Random();
 
-
-    public void initiatorRandom (View view){
-
-    }
-
-    public void duelistRandom (View view){
-
-    }
-
-    public void controllerRandom (View view){
-
-    }
-
-    public void sentinelRandom (View view){
-
+        Buttoncontroller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {imageView.setImageResource(imagescontroller[r.nextInt(imagescontroller.length)]);}
+        });
     }
 
 

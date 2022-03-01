a02a6ffffdf3655b7f41fa7b6acfa8e04d0d6a89
nerresir <bugraarslan96@gmail.com>
2022-03-01 14:27:11 +0300

    ValorantAgentPickHelper V0.0.1

---
+++ app/src/main/java/com/liontertainment/agentpicker/MainActivity.java
@@ -0,0 +1,38 @@
+package com.liontertainment.agentpicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void initiatorRandom (View view){
+
+    }
+
+    public void duelistRandom (View view){
+
+    }
+
+    public void controllerRandom (View view){
+
+    }
+
+    public void sentinelRandom (View view){
+
+    }
+
+
+
+
+
+}
